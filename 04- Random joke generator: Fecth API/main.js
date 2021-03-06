f623afa0f066f6a3bed3539b379742d72c18cbb8
Ivana Piljic <47247040+codinggrl@users.noreply.github.com>
2021-03-06 19:03:55 +0100

    Add files via upload

---
+++ 04- Random joke generator: Fecth API/main.js
@@ -0,0 +1,13 @@
+const cursor = document.querySelector('.cursor');
+
+window.addEventListener('mousemove', (e)=> {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+    cursor.setAttribute('data-fromTop', (cursor.offsetTop - scrollY))
+
+});
+
+window.addEventListener('scroll', ()=> {
+    const fromTop = parseInt(cursor.getAttribute('data-fromtop'));
+    cursor.style.top = scrollY + fromTop + 'px';
+})
